0903337aa512cd452f22e62ebd91ac46db86b33c
Pierre lulu <luttmannpierre@gmail.com>
2023-02-27 10:27:41 +0100

    Add sum function

--- utils.js
+++ utils.js
@@ -5,5 +5,17 @@ export function sayHello()
 
 export function sayGoodbye()
 {
+<<<<<<< main
 	console.log("Goodbye ");
+=======
+	console.log("Adios");
+}
+export function add(n1, n2)
+{
+	return (n1+n2);
+}
+export function minus(n1, n2)
+{
+	return (n1-n2);
+>>>>>>> Add sum function
 }
